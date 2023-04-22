adfaf245e618f593e8156572f8960ecdc115080d
Chipi012 <risedibe46@gmail.com>
2023-04-21 22:53:32 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int r = 0;
+
+	while
+		(r < 10)
+		{
+			putchar(r + '0');
+			r++;
+		}
+	putchar('\n');
+	return (0);
+}
