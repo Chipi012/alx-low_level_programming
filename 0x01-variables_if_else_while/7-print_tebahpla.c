a858037454cc273a5e2c89512f29a094c4c0407c
Chipi012 <risedibe46@gmail.com>
2023-04-23 07:28:54 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char r;
+
+	while (r >= 0)
+	{
+		putchar(r);
+		r++;
+	}
+	putchar('\n');
+	return (0);
+}
