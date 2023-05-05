b97b16bf4b3468a84a8bee194d52a54142eb0b4e
Chipi012 <risedibe46@gmail.com>
2023-05-05 16:58:27 -0700

    Reverse

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -15,12 +15,12 @@ void print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-		 if (i != 0)
-		 {
-			 printf(", ");
-		 }
-		 printf("%d", a[i]);
-		 i++;
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
 	}
 	printf("\n");
 }
